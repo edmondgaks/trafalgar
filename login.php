b84059032d3867dfa1b4c7494672960796dd205d
Edmond GAKS <edmondgaks@gmail.com>
2022-03-02 19:43:57 +0200

    working on login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 
 $username = $_POST['username'];
-$pass = $_POST['password'];
+$password = $_POST['password'];
  
 if(($username="") || ($pass="")) {
     echo "Please provide your password or username";
@@ -12,6 +12,9 @@ else {
     define("HOST","localhost");
     $DB_host = "localhost";
     $DB_user = "root";
+    $DB_password = "";
+    $DB_name = "trafalgardb";
+    $connection = mysqli_connect(HOST,$DB_user,$DB_password,$DB_name);
 }
 
 ?>
