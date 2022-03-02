deefc4244fa4a5c54a3ccbd06df86ec92b75765b
Edmond GAKS <edmondgaks@gmail.com>
2022-03-02 19:46:24 +0200

    working on login.php

--- login.php
+++ login.php
@@ -8,13 +8,25 @@ if(($username="") || ($pass="")) {
     echo "Please provide your password or username";
 }
 else {
-    $encrypt = hash("SHA512",$pass);
+    $encrypt = hash("SHA512",$password);
     define("HOST","localhost");
     $DB_host = "localhost";
     $DB_user = "root";
     $DB_password = "";
     $DB_name = "trafalgardb";
     $connection = mysqli_connect(HOST,$DB_user,$DB_password,$DB_name);
+}
+
+
+if(!$connection) {
+    echo "Connection error";
+} else {
+    $insertQuery= "INSERT INTO trafa_users(username,password) values('$username','$encrypt')";
+    $insert = mysqli_query($connection,$insertQuery) or die("Error occured".mysqli_error($connection));
+    if($insert) {
+        echo "DATA added Successfully";
+    }
+}
 }
 
 ?>
